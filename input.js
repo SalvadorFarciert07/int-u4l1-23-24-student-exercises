cbeb9a9edf0d14fb3c4320e0dff396efb62b0a86
SalvadorFarciert07 <147100085+SalvadorFarciert07@users.noreply.github.com>
2024-01-11 20:55:56 +0000

    innerhtml and vlaue stuff

--- input.js
+++ input.js
@@ -3,13 +3,18 @@ let messages = document.querySelector(".messages");
 
 // CODE ALONG, PART 1 CONTINUED
 // 3. Declare a variable to store the input element
-
+let inputTag= document.querySelector("input");
+console.log(inputTag)
 
 submitButton.addEventListener("click", function() {
     // 4. Declare a variable to store the value of the input
+  let inputValue = inputTag.value
+  console.log(inputValue)
+  
+  
     // 5. Use the console to check a value is saved.
     //  - Type something and click the button to confirm it works!
-
+messages.innerHTML = inputValue
 
     
 
@@ -18,4 +23,4 @@ submitButton.addEventListener("click", function() {
     // 7. Type something and test it out.
 
 
-});
+});;
--- storeInput.js
+++ storeInput.js
@@ -4,6 +4,12 @@ button.addEventListener("click", function() {
     let text = document.querySelector('.text').value;
     
     // 1. Save the value of the remaining three inputs in three separate variables.
+    let number = document.querySelector('.number').value;
+    let email = document.querySelector('.email').value;
+    let date = document.querySelector('.date').value;
+  console.log(number)
+  console.log(email)
+  console.log(date)
     //  - Use the text variable as a guide.
     //  - You should have a variable for the number, email, and date.
     
@@ -16,6 +22,10 @@ button.addEventListener("click", function() {
     
     
     // 2. Update the results div innerhtml to the remaining three inputs.
+    resultsDiv.innerHTML += `<p>Number: ${number}</p>`;
+    resultsDiv.innerHTML += `<p>Email: ${email}</p>`;
+    resultsDiv.innerHTML += `<p>Date: ${date}</p>`;
+  
     //  - Use the line of code above as a guide.
     //  - Take note that string interpolation is used.
     //  - Use the " += " assignment.
@@ -23,4 +33,4 @@ button.addEventListener("click", function() {
     
 
 
-});
+});;
